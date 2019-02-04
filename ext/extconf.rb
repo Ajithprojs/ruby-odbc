104b5a7685ea0f584b31394e9fb81db9a20e330d
Ajith Kumar <ajithks@vmware.com>
2019-02-04 17:40:57 +0530

    Removing backward compatibilities

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,7 +5,7 @@ if ! defined? PLATFORM
 end
 # 2.0+
 #adding this for backward compatible
-have_header('ruby/thread.h') && have_func('rb_thread_call_without_gvl', 'ruby/thread.h')
+#have_header('ruby/thread.h') && have_func('rb_thread_call_without_gvl', 'ruby/thread.h')
 # 1.9-only
 #have_func('rb_thread_blocking_region')
 
